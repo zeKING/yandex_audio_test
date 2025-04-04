f88516046f25568a1f524ba3f3e95f3b1b812036
User <arturikjan2004@gmail.com>
2025-04-04 17:17:22 +0500

    remove admin app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from app.admin.auth import authentication_backend
-from app.database import engine
+
 from app.users.router import router as router_user
 from app.audio.router import router as router_audio
-
-from sqladmin import Admin
 
 app = FastAPI()
 
@@ -14,7 +11,7 @@ app.include_router(router_user)
 app.include_router(router_audio)
 
 app.mount('/media', StaticFiles(directory='media'), name='media')
-admin = Admin(app, engine=engine, authentication_backend=authentication_backend)
+
 origins = [
     "http:localhost:5173",
     "*"
